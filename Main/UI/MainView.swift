35118781f4d988a5126003733adc4e23658009f4
Tristan <tristanchay123@gmail.com>
2024-02-03 16:41:03 +0800

    Initial Commit

---
+++ Main/UI/MainView.swift
@@ -0,0 +1,61 @@
+//
+//  MainView.swift
+//  Colourfull
+//
+//  Created by Tristan Chay on 3/2/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State private var simulateDeuteranopia = false
+    @State private var simulateProtanopia = false
+    @State private var simulateTritanopia = false
+    
+    @State private var isImagePickerPresented: Bool = false
+    @State private var selectedImage: UIImage?
+
+    var body: some View {
+        VStack {
+            if let image = selectedImage {
+                VStack {
+                    Text("Color Blindness Simulation")
+                        .font(.title)
+
+                    // Toggle buttons to enable/disable simulations
+                    Toggle("Deuteranopia", isOn: $simulateDeuteranopia)
+                        .padding()
+
+                    Toggle("Protanopia", isOn: $simulateProtanopia)
+                        .padding()
+
+                    Toggle("Tritanopia", isOn: $simulateTritanopia)
+                        .padding()
+
+                    // View with simulated colors
+                    ColorBlindnessSimulator(
+                        deuteranopia: simulateDeuteranopia,
+                        protanopia: simulateProtanopia,
+                        tritanopia: simulateTritanopia,
+                        image: image
+                    )
+                    .padding()
+                }
+            } else {
+                CameraView(isShown: $isImagePickerPresented, image: $selectedImage)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+#Preview {
+    MainView()
+}
---
+++ Main/UI/SwiftUIView.swift
@@ -0,0 +1,51 @@
+//
+//  CameraView.swift
+//  Colourfull
+//
+//  Created by Tristan Chay on 3/2/24.
+//
+
+import UIKit
+import SwiftUI
+import AVFoundation
+
+struct CameraView: UIViewControllerRepresentable {
+    @Binding var isShown: Bool
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: CameraView
+        
+        init(parent: CameraView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.isShown = false
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.isShown = false
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraView>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CameraView>) {
+    }
+}
